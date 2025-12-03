e618b5691899c7d051ea190b68853a18a7f84f60
Sergey Ivanychev <sergeyivanychev@gmail.com>
2025-12-03 15:34:13 +0100

    Adjust things after code review

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -8,26 +8,28 @@ struct Battery {
 
 #[derive(Hash, Eq, PartialEq)]
 struct DynamicProgrammingNode {
-    offset: usize,
+    remaining: usize,
     batteries_left: i32,
 }
 
-impl From<&i32> for Battery {
-    fn from(voltage: &i32) -> Self {
-        Battery { voltage: *voltage }
+impl From<u32> for Battery {
+    fn from(voltage: u32) -> Self {
+        Battery {
+            voltage: voltage as i32,
+        }
     }
 }
 
-fn find_max_comb(
+fn find_max_comb(batteries: &[Battery], comb_size: usize) -> Option<i64> {
+    let mut cache: HashMap<DynamicProgrammingNode, i64> = HashMap::new();
+    find_max_comb_impl(batteries, comb_size, &mut cache)
+}
+
+fn find_max_comb_impl(
     batteries: &[Battery],
     comb_size: usize,
-    answers: Option<&mut HashMap<DynamicProgrammingNode, i64>>,
+    answers: &mut HashMap<DynamicProgrammingNode, i64>,
 ) -> Option<i64> {
-    let answers = match answers {
-        Some(map) => map,
-        None => &mut HashMap::new(),
-    };
-
     if comb_size == 0 {
         return Some(0);
     }
@@ -36,15 +38,15 @@ fn find_max_comb(
     }
 
     let node = DynamicProgrammingNode {
-        offset: batteries.len(),
+        remaining: batteries.len(),
         batteries_left: comb_size as i32,
     };
     if let Some(&answer) = answers.get(&node) {
         return Some(answer);
     }
 
-    let not_taken = find_max_comb(&batteries[1..], comb_size, Some(answers));
-    let taken = find_max_comb(&batteries[1..], comb_size - 1, Some(answers))
+    let not_taken = find_max_comb_impl(&batteries[1..], comb_size, answers);
+    let taken = find_max_comb_impl(&batteries[1..], comb_size - 1, answers)
         .map(|v| 10i64.pow(comb_size as u32 - 1) * batteries[0].voltage as i64 + v);
 
     let values = [not_taken, taken];
@@ -62,17 +64,16 @@ pub fn main(args: &Args) {
             .iter()
             .map(|line| {
                 line.chars()
-                    .map(|c| c.to_digit(10).unwrap() as i32)
-                    .collect::<Vec<i32>>()
+                    .map(|c| c.to_digit(10).unwrap().into())
+                    .collect::<Vec<Battery>>()
             })
-            .map(|vec| vec.iter().map(Battery::from).collect())
             .collect();
 
     println!("Battery count {}, combs: {}", battery_lines[0].len(), combs);
 
     let total_voltage: i64 = battery_lines
         .iter()
-        .map(|batteries| find_max_comb(batteries, combs, None).unwrap())
+        .map(|batteries| find_max_comb(batteries, combs).unwrap())
         .sum();
     println!("Total voltage: {}", total_voltage);
 }
